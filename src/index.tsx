67cf5e6126b4cc4947640d226da684c678a996bb
themetronome <vasyayp@yandex.ru>
2020-10-27 23:01:09 +0400

    initial

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React, { ReactElement } from 'react'
+import ReactDOM from 'react-dom'
+
+const App = (): ReactElement => {
+  return <h1>test!</h1>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
