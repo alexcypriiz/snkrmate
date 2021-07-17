97a9b2c2eabb676d34cfc69a73bf3ea8337f362f
themetronome <vasyayp@yandex.ru>
2021-07-17 12:09:34 +0400

    chore: remove older env

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React, { ReactElement } from 'react'
-import ReactDOM from 'react-dom'
-
-const App = (): ReactElement => {
-  return <h1>test!</h1>
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
