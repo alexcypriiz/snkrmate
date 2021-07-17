e69fff47b2c1e64cd7ef36e4e08fbd49746aa86f
themetronome <vasyayp@yandex.ru>
2021-07-17 12:04:59 +0400

    chore: change frontend stack

---
+++ frontend/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>test!</div>
+</template>
+
+<script lang="ts">
+import { Vue } from "vue-class-component";
+
+console.log("test");
+
+export default class App extends Vue {}
+</script>
+
+<style></style>
